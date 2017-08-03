c7d4ea06da417552337201b9c658018a1c7d0ff8
CyberWolf37 <borel.loic.37@gmail.com>
2017-08-03 13:46:38 +0200

    First commit to push my project

---
+++ Police/release/Media/Map/Tiled_01.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tile01" tilewidth="16" tileheight="16" tilecount="768" columns="32">
+ <image source="../../../../../../Pictures/Image Game/tilea2.png" width="512" height="384"/>
+ <terraintypes>
+  <terrain name="Plaine_verte" tile="163"/>
+ </terraintypes>
+ <tile id="0" terrain=",0,,"/>
+</tileset>
